c2d8a22cdcea05dcfadc28d7d2d64bd2af67bbe0
shiva <shivamoeini2000@gmail.com>
2022-04-06 16:18:03 +0430

    cmit5

--- js/details.js
+++ js/details.js
@@ -1,6 +1,8 @@
 // javascript for details.html
 const id = new URLSearchParams(window.location.search).get("id");
 const container = document.querySelector(".details");
+const deleteBtn = document.querySelector(".delete");
+
 const renderDetails = async () => {
   const res = await fetch("http://localhost:3000/posts/" + id);
   const post = await res.json();
@@ -9,4 +11,10 @@ const renderDetails = async () => {
   <p>${post.body}</p>`;
   container.innerHTML = template;
 };
+deleteBtn.addEventListener("click", async (e) => {
+  const res = await fetch("http://localhost:3000/posts/" + id, {
+    method: "DELETE",
+  });
+  window.location.replace("index.html");
+});
 window.addEventListener("DOMContentLoaded", () => renderDetails());
